8e7130c00f5a6e00b1336be26b03b4eddddffac2
Lapis, Kasen <neptune.mk2@gmail.com>
2024-07-21 00:04:19 +0900

    add new nvim plugin: hlchunk

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,17 @@
+return
+{
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "VeryLazy" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true
+        },
+        indent = {
+          enable = true
+        }
+      })
+    end
+  },
+}
